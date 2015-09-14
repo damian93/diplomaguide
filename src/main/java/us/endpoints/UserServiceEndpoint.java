58c5430f9e3f35f869b9c7390ce56e77ad9b50d5
damian93 <>
2015-09-14 12:51:56 +0200

    Dodanie javadoców do USM endpoint oraz USM endpoint local

--- src/main/java/us/endpoints/UserServiceEndpoint.java
+++ src/main/java/us/endpoints/UserServiceEndpoint.java
@@ -37,54 +37,122 @@ public class UserServiceEndpoint implements UserServiceEndpointLocal {
     @EJB
     AdminManagerLocal adminManagerLocal;
 
+    /**
+     * Deleguje utworzenie(rejestracje) użytkownika do
+     * UnauthenticadedUserManagera
+     *
+     * @param user obiekt użytkownika do rejestracji
+     * @param type nazwa roli użytkownika
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     @Override
     public void createUser(Users user, String type) throws BusinessException {
         unauthenticatedUserManagerLocal.createUser(user, type);
 
     }
 
+    /**
+     * Deleguje pobranie listy wszystkich poziomów dostępów(ról), a następnie ją
+     * zwraca
+     *
+     * @return lista poziomów dostępów(ról)
+     */
     @Override
     public List<Accesslevelsdictionary> getAllAccessLevels() {
         return unauthenticatedUserManagerLocal.getAllAccessLevels();
 
     }
 
+    /**
+     * Deleguje pobranie użytkownika do edycji do AuthenticatedUserManagera.
+     * Zapamiętuje stan pobranego obiektu oraz zwraca ten obiekt
+     *
+     * @param name login użytkownika do edycji
+     * @return pobrany obiekt użytkownika do edycji
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     @Override
     public Users getUserToEdit(String name) throws BusinessException {
         userState = authenticatedUserManagerLocal.getUserToEdit(name);
         return userState;
     }
 
+    /**
+     * Deleguje pobranie użytkownika o danym loginie do
+     * AuthenticatedUserManagera, a następnie zwraca pobrany obiekt
+     *
+     * @param name login użytkownika
+     * @return obiekt użytkownika o danym loginie
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     @Override
     public Users getUser(String name) throws BusinessException {
         return authenticatedUserManagerLocal.getUser(name);
 
     }
 
+    /**
+     * Deleguje pobranie listy wszystkich uzytkowników systemu do AdminManagera,
+     * a następnie zwraca pobraną listę
+     *
+     * @return lista wszystkim użytkowników systemu
+     */
     @Override
     public List<Users> getUsersList() {
         return adminManagerLocal.getUsersList();
 
     }
 
+    /**
+     * Deleguje pobranie użytkownika do edycji przez administratora do
+     * AdminManagera. Zapamiętuje jego stan oraz zwraca pobrany obiekt
+     *
+     * @param rowData obiekt użytkownika pobieranego do edycji przez
+     * administratora
+     * @return obiekt użytkownika do edycji
+     */
     @Override
     public Users getUser(Users rowData) {
         userState = adminManagerLocal.getUser(rowData);
         return userState;
     }
 
+    /**
+     * Deleguje edycję użytkownika przez administratora do metody AdminManagera
+     *
+     * @param userToEdit użytkownik do edycji przez administratora
+     * @param oldPassword stare hasło
+     * @param newPassword nowe hasło
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     @Override
     public void editUserByAdmin(Users userToEdit, String oldPassword, String newPassword) throws BusinessException {
         adminManagerLocal.editUserByAdmin(userState, userToEdit, oldPassword, newPassword);
         userState = null;
     }
 
+    /**
+     * Deleguje pobranie listy użytkowników zawierających szukaną frazę w
+     * loginie do metody AdminManagera a następnie zwraca pobraną listę
+     *
+     * @param matchLogin fraza szukana w loginie
+     * @return lista użytkowników zawierąca w loginie szukaną frazę
+     */
     @Override
     public List<Users> filter(String matchLogin) {
         return adminManagerLocal.filter(matchLogin);
 
     }
 
+    /**
+     * Deleguje edycję swoich danych przez użytkownika do dedykowanej metody
+     * AuthenticatedManagera
+     *
+     * @param userOldPassword stare hasło
+     * @param userNewPassword nowe hasło
+     * @param authorizedUser edytowany obiekt użytkownika
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     @Override
     public void editUser(String userOldPassword, String userNewPassword, Users authorizedUser) throws BusinessException {
         authenticatedUserManagerLocal.editUser(userOldPassword, userNewPassword, authorizedUser, userState);
@@ -92,6 +160,12 @@ public class UserServiceEndpoint implements UserServiceEndpointLocal {
 
     }
 
+    /**
+     * Deleguje pobranie listy wszystkich dostępnych tytułów(stopni naukowych)
+     * do AuthenticatedManagera, a następnie zwraca pobraną listę
+     *
+     * @return lista wszystkich dostępnych tytułów(stopni naukowych)
+     */
     @Override
     public List<Degrees> getDegreeList() {
         return authenticatedUserManagerLocal.getDegreesList();
--- src/main/java/us/endpoints/UserServiceEndpointLocal.java
+++ src/main/java/us/endpoints/UserServiceEndpointLocal.java
@@ -19,24 +19,98 @@ import javax.ejb.Local;
 @Local
 public interface UserServiceEndpointLocal {
 
+    /**
+     * Deleguje utworzenie(rejestracje) użytkownika do
+     * UnauthenticadedUserManagera
+     *
+     * @param user obiekt użytkownika do rejestracji
+     * @param type nazwa roli użytkownika
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     void createUser(Users user, String type) throws BusinessException;
 
+    /**
+     * Deleguje pobranie listy wszystkich poziomów dostępów(ról), a następnie ją
+     * zwraca
+     *
+     * @return lista poziomów dostępów(ról)
+     */
     List<Accesslevelsdictionary> getAllAccessLevels();
 
+    /**
+     * Deleguje pobranie użytkownika o danym loginie do
+     * AuthenticatedUserManagera, a następnie zwraca pobrany obiekt
+     *
+     * @param name login użytkownika
+     * @return obiekt użytkownika o danym loginie
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     Users getUser(String name) throws BusinessException;
 
+    /**
+     * Deleguje pobranie listy wszystkich uzytkowników systemu do AdminManagera,
+     * a następnie zwraca pobraną listę
+     *
+     * @return lista wszystkim użytkowników systemu
+     */
     List<Users> getUsersList();
 
+    /**
+     * Deleguje pobranie użytkownika do edycji przez administratora do
+     * AdminManagera. Zapamiętuje jego stan oraz zwraca pobrany obiekt
+     *
+     * @param rowData obiekt użytkownika pobieranego do edycji przez
+     * administratora
+     * @return obiekt użytkownika do edycji
+     */
     Users getUser(Users rowData);
 
+    /**
+     * Deleguje edycję użytkownika przez administratora do metody AdminManagera
+     *
+     * @param userToEdit użytkownik do edycji przez administratora
+     * @param oldPassword stare hasło
+     * @param newPassword nowe hasło
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     void editUserByAdmin(Users userToEdit, String oldPassword, String newPassword) throws BusinessException;
 
+    /**
+     * Deleguje pobranie listy użytkowników zawierających szukaną frazę w
+     * loginie do metody AdminManagera a następnie zwraca pobraną listę
+     *
+     * @param matchLogin fraza szukana w loginie
+     * @return lista użytkowników zawierąca w loginie szukaną frazę
+     */
     List<Users> filter(String matchLogin);
 
+    /**
+     * Deleguje edycję swoich danych przez użytkownika do dedykowanej metody
+     * AuthenticatedManagera
+     *
+     * @param userOldPassword stare hasło
+     * @param userNewPassword nowe hasło
+     * @param authorizedUser edytowany obiekt użytkownika
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     void editUser(String userOldPassword, String userNewPassword, Users authorizedUser) throws BusinessException;
-    
+
+    /**
+     * Deleguje pobranie użytkownika do edycji do AuthenticatedUserManagera.
+     * Zapamiętuje stan pobranego obiektu oraz zwraca ten obiekt
+     *
+     * @param name login użytkownika do edycji
+     * @return pobrany obiekt użytkownika do edycji
+     * @throws BusinessException wyjątek aplikacyjny
+     */
     Users getUserToEdit(String name) throws BusinessException;
 
+    /**
+     * Deleguje pobranie listy wszystkich dostępnych tytułów(stopni naukowych)
+     * do AuthenticatedManagera, a następnie zwraca pobraną listę
+     *
+     * @return lista wszystkich dostępnych tytułów(stopni naukowych)
+     */
     List<Degrees> getDegreeList();
 
 }
